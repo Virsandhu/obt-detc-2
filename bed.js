24f6a5d6551b94eec94f32861c8eba0f7187eda4
Virsandhu <80320001+Virsandhu@users.noreply.github.com>
2022-04-26 20:56:08 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,27 @@
+function preload(){
+    loadImage("Virat-Kohli-and-his-Cars-01.jpg")
+}
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+
+    objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting objects";
+
+}
+function modelLoaded(){
+    console.log("model loaded");
+    status= true;
+    
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+        
+    }
+        console.log(results);
+        objects=results;
+    }
+    
